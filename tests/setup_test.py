83230e7b5c8a8fd3119b85be7b01ef05a51a4a0a
Samuel Greenwood <earsgre@foe-linux-03.leeds.ac.uk>
2022-09-01 14:58:29 +0100

    updated tests for CLI with pytest

--- tests/setup_test.py
+++ tests/setup_test.py
@@ -1,22 +1,29 @@
-import thermal_history as th
+def test_setup():
 
-#Create parameters class with just those necessary for 'simple_test' core method.
-#This shortcut saves needing a parameters file for this test case.
-#Inherit from th.model.Parameters to get built in constants
-class test_parameters(th.model.Parameters):
 
-    #Redefine init function as we are not specifying any input file, just including
-    #the params needed for this test.
-    def __init__(self):
-        self.core         = True
-        self.mantle       = False
-        self.stable_layer = False
+    import thermal_history as th
 
-        self.rc      = 1  #Radius of the core
-        self.density = 1  #Core density
-        self.cp      = 1  #Specific heat capacity
-        self.T       = 1  #Initial temperature of the core
+    #Create parameters class with just those necessary for 'simple_test' core method.
+    #This shortcut saves needing a parameters file for this test case.
+    #Inherit from th.model.Parameters to get built in constants
+    class example_parameters(th.model.Parameters):
 
-prm = test_parameters()
+        #Redefine init function as we are not specifying any input file, just including
+        #the params needed for this test.
+        def __init__(self):
+            self.core         = True
+            self.mantle       = False
+            self.stable_layer = False
 
-model = th.model.setup_model(prm, core_method='simple_test', verbose=False)
+            self.rc      = 1  #Radius of the core
+            self.density = 1  #Core density
+            self.cp      = 1  #Specific heat capacity
+            self.T       = 1  #Initial temperature of the core
+
+    prm = example_parameters()
+
+    model = th.model.setup_model(prm, core_method='simple_test', verbose=False)
+
+
+if __name__ == '__main__':
+    test_setup()
