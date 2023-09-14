6b210b7560517f24867d5abe48c823f73e529634
Chris Davies <c.davies@leeds.ac.uk>
2023-09-14 16:49:06 +0100

    Updated KW/profiles - just a couple of comments

--- thermal_history/mantle_models/knibbe_westrenen18/routines/melting.py
+++ thermal_history/mantle_models/knibbe_westrenen18/routines/melting.py
@@ -12,27 +12,30 @@ def mantle_melt(D_ref, D_crust, r, rho, g, T, solidus_params, liquidus_params):
     P_fine = prof.pressure(r_fine, rho_fine, g)
     T_fine = np.interp(r_fine, r, T)
 
+    # KvW eqn A2
     T_sol = prof.solidus(P_fine, solidus_params) + (D_crust/D_ref)* 150 
     T_liq = prof.liquidus(P_fine, liquidus_params) 
 
     mask = np.zeros(r_fine.size)
     mask[np.where(T_fine > T_sol)[0]] = 1
-
+    
     #Keep melt fraction at <= 1
     T_fine[T_fine>T_liq] = T_liq[T_fine>T_liq]
-
+    
     # V_melt = 4*np.pi*trapezoid(mask*r**2,x=r)
     V_melt = 4*np.pi*trapezoid(r_fine, mask*r_fine**2)[-1]
-
+    
     if V_melt == 0:
         ma = 0
         dm_dT = 0
         melting_radii = np.array([np.nan, np.nan])
     else:
         # ma = 1/V_melt * 4*np.pi*trapezoid( mask*r**2 *(T-T_sol) / (T_liq-T_sol) , x=r)
+        # kvW eqn A4
+            
         ma = 1/V_melt * 4*np.pi*trapezoid(r_fine, mask*r_fine**2 *(T_fine-T_sol) / (T_liq-T_sol))[-1]
         melting_radii = np.array([r_fine[T_fine>T_sol][0], r_fine[T_fine>T_sol][-1]])
-
+        
         #Increase T by 1 degree and recalc ma to get dma_dT
         T2 = np.interp(r_fine, r, T+1)
         mask2 = np.zeros(r_fine.size)
