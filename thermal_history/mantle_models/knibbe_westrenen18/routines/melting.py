19569721123c93e3f09b7c9c250f4b1c73b8ba7e
Chris Davies <c.davies@leeds.ac.uk>
2023-11-22 10:25:44 +0000

    If melt region is undefined return a number that can be checked so solution can terminate cleanly

--- thermal_history/mantle_models/knibbe_westrenen18/routines/melting.py
+++ thermal_history/mantle_models/knibbe_westrenen18/routines/melting.py
@@ -5,13 +5,13 @@ from . import profiles as prof
 import matplotlib.pyplot as plt
 
 def mantle_melt(D_ref, D_crust, r, rho, g, T, solidus_params, liquidus_params):
-
+        
     #Increase grid resolution to accurately identify melting zones
     r_fine = linspace(r[0], r[-1], 100)
     rho_fine = np.interp(r_fine, r, rho)
     P_fine = prof.pressure(r_fine, rho_fine, g)
     T_fine = np.interp(r_fine, r, T)
-
+    
     # KvW eqn A2
     T_sol = prof.solidus(P_fine, solidus_params) + (D_crust/D_ref)* 150 
     T_liq = prof.liquidus(P_fine, liquidus_params) 
@@ -21,7 +21,7 @@ def mantle_melt(D_ref, D_crust, r, rho, g, T, solidus_params, liquidus_params):
     
     #Keep melt fraction at <= 1
     T_fine[T_fine>T_liq] = T_liq[T_fine>T_liq]
-    
+        
     # V_melt = 4*np.pi*trapezoid(mask*r**2,x=r)
     V_melt = 4*np.pi*trapezoid(r_fine, mask*r_fine**2)[-1]
     
@@ -32,6 +32,10 @@ def mantle_melt(D_ref, D_crust, r, rho, g, T, solidus_params, liquidus_params):
     else:
         # ma = 1/V_melt * 4*np.pi*trapezoid( mask*r**2 *(T-T_sol) / (T_liq-T_sol) , x=r)
         # kvW eqn A4
+        
+        if len(r_fine[T_fine>T_sol]) == 0:
+            return -1.0, -1.0, -1.0, -1.0, \
+                {'r': r_fine,'rho': rho_fine,'P': P_fine,'T': T_fine,'T_liq': T_liq,'T_sol': T_sol}
             
         ma = 1/V_melt * 4*np.pi*trapezoid(r_fine, mask*r_fine**2 *(T_fine-T_sol) / (T_liq-T_sol))[-1]
         melting_radii = np.array([r_fine[T_fine>T_sol][0], r_fine[T_fine>T_sol][-1]])
@@ -52,6 +56,5 @@ def mantle_melt(D_ref, D_crust, r, rho, g, T, solidus_params, liquidus_params):
                 'T': T_fine,
                 'T_liq': T_liq,
                 'T_sol': T_sol}
-
 
     return V_melt, ma, melting_radii, dm_dT, profiles
